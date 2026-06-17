588a28d0b95ca0b6b6010fe28f6be0d7a8e0c963
albanm297-collab <albanm297@gmail.com>
2026-06-17 18:18:30 -0500

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,8 @@
+import sqlite3
 
+def init_db():
+    conn = sqlite3.connect('laboratorio.db')
+    cursor = conn.cursor()
+    # Aquí puedes dejar que cree las tablas de tu laboratorio
+    conn.commit()
+    conn.close()
